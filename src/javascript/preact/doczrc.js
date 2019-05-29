08156770fa19a2f8f19a8adeacc876803f3ec317
zhaopeng <719197352@qq.com>
2019-05-29 15:04:30 +0800

    添加 preact 源码分析

---
+++ src/javascript/preact/doczrc.js
@@ -0,0 +1,12 @@
+
+export default {
+    title: 'preact 源码解析',
+    port: 8082,
+    typescript: true,
+    codeSandbox: false,
+    open: true,
+    menu: [
+      { name: 'Hooks' },
+      { name: 'Components'}
+    ],
+}
